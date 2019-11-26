de596b76c49ab664012b49679f89702e9b5d0767
phunguyen <57187872+phumapuit@users.noreply.github.com>
2019-11-26 23:29:38 +0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,216 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <set>
+#include <fstream>
+using namespace std;
+
+// C++ program for Huffman Coding 
+
+// A Huffman tree node 
+struct MinHeapNode {
+
+	// One of the input characters 
+	char data;
+
+	// Frequency of the character 
+	unsigned freq;
+
+	// Left and right child 
+	MinHeapNode *left, *right;
+
+	MinHeapNode(char data, unsigned freq)
+
+	{
+
+		left = right = nullptr;
+		this->data = data;
+		this->freq = freq;
+	}
+};
+
+// For comparison of 
+// two heap nodes (needed in min heap) 
+struct compare {
+
+	bool operator()(MinHeapNode* l, MinHeapNode* r)
+
+	{
+		return (l->freq > r->freq);
+	}
+};
+
+// Prints huffman codes from 
+// the root of Huffman Tree. 
+void printCodes(struct MinHeapNode* root, string str)
+{
+
+	if (!root)
+		return;
+
+	if (root->data != '$')
+		cout << root->data << ": " << str << "\n";
+
+	printCodes(root->left, str + "0");
+	printCodes(root->right, str + "1");
+}
+
+// The main function that builds a Huffman Tree and 
+// print codes by traversing the built Huffman Tree 
+void HuffmanCodes(vector<char> data, vector<int> freq, int size)
+{
+	struct MinHeapNode *left, *right, *top;
+
+	// Create a min heap & inserts all characters of data[] 
+	priority_queue<MinHeapNode*, vector<MinHeapNode*>, compare> minHeap;
+
+	for (int i = 0; i < size; ++i)
+		minHeap.push(new MinHeapNode(data[i], freq[i]));
+
+	// Iterate while size of heap doesn't become 1 
+	while (minHeap.size() != 1) {
+
+		// Extract the two minimum 
+		// freq items from min heap 
+		left = minHeap.top();
+		minHeap.pop();
+
+		right = minHeap.top();
+		minHeap.pop();
+
+		// Create a new internal node with 
+		// frequency equal to the sum of the 
+		// two nodes frequencies. Make the 
+		// two extracted node as left and right children 
+		// of this new node. Add this node 
+		// to the min heap '$' is a special value 
+		// for internal nodes, not used 
+		top = new MinHeapNode('$', left->freq + right->freq);
+
+		top->left = left;
+		top->right = right;
+
+		minHeap.push(top);
+	}
+
+	// Print Huffman codes using 
+	// the Huffman tree built above 
+	printCodes(minHeap.top(), "");
+}
+
+//char* letters(const string& s) {
+//	char* p = new char[s.length()];
+//	// tao bang ma ASCII
+//	vector<char> ascii{};
+//	for (auto i = 0; i <= 127; ++i) {
+//		ascii.push_back(char(i));
+//	}
+//	//
+//	
+//	for (size_t j = 0; j < s.length(); ++j) {
+//		for (auto i = 0; i < ascii.size(); ++i) {
+//
+//		}
+//	}
+//	
+//	
+//	return p;
+//}
+//
+//int* freq(const char* s) {
+//
+//	/*int numberOfChar = 0;*/
+//	// so phan tu cua chuoi s
+//	int size = sizeof(s) / sizeof(s[0]);
+//
+//	// tao bang ma ascii
+//	vector<char> ascii{};
+//	for (auto i = 0; i <= 127; ++i) {
+//		ascii.push_back(char(i));
+//	}
+//	// tao bang chua tan suat xuat hien cua cac ky tu
+//	int* p = new int[1];
+//
+//	for (auto i = 0; i <= 127; ++i) {
+//		int count = 0;
+//		for (auto j = 0; j < size; ++j) {
+//			if (s[j] == ascii[i]) {
+//				++count;
+//			}
+//			else {
+//				break;
+//			}
+//		}
+//
+//	}
+//	
+//	
+//
+//	return p;
+//}
+// Driver program to test above functions 
+
+void _freq(const string& s, vector<char> &arr, vector<int> &freq) {
+	std::vector<char> ascii{};
+	for (auto i = 0; i <= 127; ++i) {
+		ascii.push_back(char(i));
+	}
+
+	std::set<char> arrChar{};
+	for (size_t i = 0; i < s.length(); ++i) {
+		for (size_t j = 0; j < ascii.size(); ++j) {
+			if (s[i] == ascii[j]) {
+				arrChar.insert(s[i]);
+				break;
+			}
+		}
+	}
+
+	int count = 0;
+	std::vector<int> freqNum{};
+	for (auto it = arrChar.begin(); it != arrChar.end(); ++it) {
+		for (size_t i = 0; i < s.length(); ++i) {
+			if (*it == s[i]) {
+				++count;
+			}
+		}
+		freqNum.push_back(count);
+		count = 0;
+	}
+	int i = 0;
+	for (auto it = arrChar.begin(); it != arrChar.end(); ++it) {
+		cout << *it << ": " << freqNum[i] << '\n';
+		arr.push_back(*it);
+		freq.push_back(freqNum[i]);
+		++i;
+	}
+}
+
+int main()
+{
+	std::string s{};
+	std::cout << "Nhap cai gi do de ma hoa(Huffman): " << '\n';
+	getline(cin, s);
+	/*string line{};
+	ifstream myfile("A.txt");
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			line += line;
+		}
+		myfile.close();
+	}
+	else cout << "Unable to open file";
+	cout << line;*/
+	vector<char> arr{};
+	vector<int> freq{};
+	cout << "Tan suat xuat hien: " << '\n';
+	_freq(s, arr, freq);
+	size_t size = arr.size();
+	cout << "Sau khi ma hoa: " << '\n';
+	HuffmanCodes(arr, freq, size);
+	return 0;
+}
+
+// This code is contributed by Aditya Goel 
